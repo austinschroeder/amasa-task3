580fa6818c3bb8d8bbd70faaaa96fb42f4d99e5d
austinschroeder <austinsoundy@gmail.com>
2021-11-06 15:53:25 -0700

    finished styling

--- main.js
+++ main.js
@@ -7,19 +7,22 @@ const getArtistTemplate = (artist) => {
   return `
 
     <div class="col-md-12">
-
-      <div class="card mb-3">
-        <img src="${artist.strArtistBanner}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Genre = "${artist.strGenre}"</h5>
-          <p class="card-text"></p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="card mb-3">
+          <img src="${artist.strArtistBanner}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${artist.strArtist}</h5>
+            <p class="card-text">Year Formed: ${artist.intFormedYear}</p>
+            <p class="card-text">Genre: ${artist.strGenre}</p>
+            <p class="card-text">Label: ${artist.strLabel}</p>
+            <a href="https://${artist.strWebsite}" class="btn btn-secondary" target="_blank">Go website</a>
+            <p class="card-text">Bio: ${artist.strBiographyEN}</p>
+          </div>
         </div>
       </div>
-
-    </div>
   `;
 };
+
+// ${artist.strWebsite}
 
 const renderArtists = (artistArr) => {
   artistTemplates = [];
